70596b1b674fba640e8e49bbdd2bdb7587c40102
Thiago2123 <thi_meneghetti@hotmail.com>
2019-09-09 10:12:31 -0300

    bootstatp

--- def/head.php
+++ def/head.php
@@ -1,14 +1,13 @@
 <!-- bibliotecas -->
 <meta charset="UTF-8"/>
 <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, width=device-width"/>
-<link href="style/css/def/main.css" rel="stylesheet" type="text/css">
+ <link href="style/css/def/main.css" rel="stylesheet" type="text/css">
 <link href="lib/animate.css-master/animate.min.css" rel="stylesheet" type="text/css">
 <link href="lib/fontawesome-5.9.0/css/all.css" rel="stylesheet" type="text/css">
 <script src="lib/jquery-3.4.1/jquery-3.4.1.min.js" type="text/javascript"></script>
 <script src="lib/bootstrap-4.3.1/dist/js/bootstrap.min.js"></script>
 <script src="lib/bootstrap-notify-3.1.3/dist/bootstrap-notify.min.js"></script>
 
-<!-- componentes -->
-
+<!-- componentes
 <link href="style/css/com/button.css" rel="stylesheet" type="text/css" />
-<link href="style/css/com/tabela.css" rel="stylesheet" type="text/css" />
+<link href="style/css/com/tabela.css" rel="stylesheet" type="text/css" /> -->
---
+++ view/home.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <?php 
+            require("../def/head.php");
+            require("../def/function.php");
+            $connection = connection();
+        
+        ?>
+        <!-- <script type="text/javascript" src="js/livro.js"></script> -->
+    </head>
+    <body class="bg-gradient-info">
+    <button id="btn-novo" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Incluir Novo</button>
+    </body>
+</html>
