73457fc6de6ad3ab88f00bc3d112c523592712ef
Thiago2123 <thi_meneghetti@hotmail.com>
2019-09-12 09:31:57 -0300

    nav

--- index.php
+++ index.php
@@ -4,3 +4,4 @@ header("Location: home");
 
 
 // compilar o css: http://localhost/Projeto/ProjectLOL/style/compileSass.php
+// url da pagina: http://localhost/Projeto/ProjectLOL/home
--- view/home.php
+++ view/home.php
@@ -9,6 +9,12 @@
         <!-- <script type="text/javascript" src="js/livro.js"></script> -->
     </head>
     <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <form class="form-inline row">
+                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+        </nav>
         <button id="btn-novo" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Incluir Novo</button>
     </body>
 </html>
