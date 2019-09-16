b27cfce04dc4121ccf9c99321f87d43e7d8eb865
Thiago2123 <thi_meneghetti@hotmail.com>
2019-09-16 09:27:04 -0300

    nav

--- view/home.php
+++ view/home.php
@@ -4,17 +4,18 @@
             require("../def/head.php");
             require("../def/function.php");
             $connection = connection();
-        
         ?>
         <!-- <script type="text/javascript" src="js/livro.js"></script> -->
     </head>
     <body>
-        <nav class="navbar navbar-dark bg-dark">
-            <form class="form-inline row">
-                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Pesquisar</button>
-            </form>
-        </nav>
-        <button id="btn-novo" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Incluir Novo</button>
+        <div class="container-fluid no-gutters">
+            <nav class="navbar navbar-dark bg-dark">
+                <div class=" row">
+                    <input class="mr-sm-2" placeholder="Pesquisar" aria-label="Pesquisar">
+                    <button class="btn btn-outline-primary my-2 my-sm-0">Pesquisar</button>
+                </div>
+            </nav>
+            <button id="" class="btn btn-primary"><i class="fas fa-plus-circle"></i> Incluir Novo</button>
+        </div>
     </body>
 </html>
