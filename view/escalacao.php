a2db7009dc6d712fb4dfae0fcc243b8fa3c8fd19
Thiago2123 <thi_meneghetti@hotmail.com>
2019-10-07 11:19:35 -0300

    Tabela/escalaçao

--- view/escalacao.php
+++ view/escalacao.php
@@ -61,12 +61,90 @@
                                 <option value="kabum">Kabum</option>
                             </select>
                         </div>
-                        <div class="form-group col-8">
-                            <label for="preco">Preço</label>
-                            <input type="range" class="custom-range" min="0" max="100" id="preco">
+                        <div class="form-group col-4">
+                            <fieldset class="form-group">
+                                <div class="row">
+                                    <legend class="col-form-label col-sm-2 pt-0">Preço:</legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                            <label class="form-check-label" for="gridRadios1">
+                                                Abaixo de 10$
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                            <label class="form-check-label" for="gridRadios2">
+                                                Maior de 10$
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+                                            <label class="form-check-label" for="gridRadios3">
+                                                Maiores de 20$
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
                         </div>
                     </div>
-                   
+                    <table class="table table-striped table-dark col-10">
+                        <thead>
+                            <tr>
+                            <th style="width: 10%">Nome</th>
+                            <th style="width: 10%">Time</th>
+                            <th style="width: 5%">Rota</th>
+                            <th style="width: 5%">Valor</th>
+                            <th style="width: 2%"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Brtt</td>
+                                <td>Flamengo</td>
+                                <td>Atirador</td>
+                                <td>25</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+
+                            </tr>
+                            <tr>
+                                <td>Micão</td>
+                                <td>Intz</td>
+                                <td>Atirador</td>
+                                <td>22</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+                            </tr>
+                            <tr>
+                                <td>Robo</td>
+                                <td>Flamengo</td>
+                                <td>Topo</td>
+                                <td>2</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+                            </tr>
+                            <tr>
+                                <td>Revolta</td>
+                                <td>Keyd</td>
+                                <td>Selva</td>
+                                <td>10</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+                            </tr>
+                            <tr>
+                                <td>Luci</td>
+                                <td>Flamengo</td>
+                                <td>Suporte</td>
+                                <td>20</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+                            </tr>
+                            <tr>
+                                <td>Kami</td>
+                                <td>Pain</td>
+                                <td>Meio</td>
+                                <td>37</td>
+                                <td><i class="fas fa-shopping-cart"  style="cursor:pointer"></i></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div class="col-4">b</div>
             </div>
