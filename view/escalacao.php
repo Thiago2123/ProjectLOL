120ddf27795cb451a153accec63d8ea24c260a27
Thiago2123 <thi_meneghetti@hotmail.com>
2019-10-07 14:25:34 -0300

    tab

--- view/escalacao.php
+++ view/escalacao.php
@@ -19,7 +19,7 @@
                 <div>
                     <ul class="nav nav-pills nav-fill">
                         <li class="nav-item ">
-                            <a class="nav-link text-warning" href="">Página Inicial</a>
+                            <a class="nav-link text-warning" href="home">Página Inicial</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white active" href="">Escalação</a>
@@ -39,11 +39,12 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-8">
-                    <div id="filtros" class="border-warning row">
+                <div class="col-8 mt-4">
+                    <div id="filtros" class="border border-warning row">
                         <div class="form-group col-2">
                             <label for="Rota">Rota</label>
                             <select class="form-control" id="Rota">
+                                <option value=""></option>
                                 <option value="topo">Topo</option>
                                 <option value="selva">Selva</option>
                                 <option value="meio">Meio</option>
@@ -54,6 +55,7 @@
                         <div class="form-group col-2">
                             <label for="Rota">Time</label>
                             <select class="form-control" id="Rota">
+                                <option value=""></option>
                                 <option value="pain">Pain</option>
                                 <option value="cnb">Cnb</option>
                                 <option value="keyd">Keyd</option>
@@ -146,7 +148,125 @@
                         </tbody>
                     </table>
                 </div>
-                <div class="col-4">b</div>
+                <div class="col-4 mt-2">
+                    <div class="accordion" id="accordionExample">
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <i class="fas fa-plus"></i> Time 1
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                <div class="card-body">
+                                <table class="table-xs table-striped table-dark col-10">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 10%">Nome</th>
+                                            <th style="width: 5%">Rota</th>
+                                            <th style="width: 12%"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Mylon</td>
+                                            <td>Topo</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R">Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Minerva</td>
+                                            <td>Selva</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R">Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Kami</td>
+                                            <td>Meio</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R">Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Diud</td>
+                                            <td>Suporte</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R">Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Brtt</td>
+                                            <td>Atirador</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R">Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Robo</td>
+                                            <td>Topo</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R" selected>Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Yoda</td>
+                                            <td>Meio</td>
+                                            <td><select class="form-control" id="reserva">
+                                                    <option value="T">Titular</option>
+                                                    <option value="R" selected>Reserva</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <i class="fas fa-plus"></i> Time 2
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingThree">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <i class="fas fa-plus"></i> Time 3
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
