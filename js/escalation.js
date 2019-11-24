7b06a0286ba459b8774b28bc7978de07d5790937
Pedro silva <pedrod.ss@hotmail.com>
2019-11-24 14:03:10 -0300

    Changes
    changes demais

---
+++ js/escalation.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+	createPlayerTable()
+}
+
+function getPlayers (){
+	var players = getFullDocument('players');
+	return players;
+}
+
+function createPlayerTable (){
+	var testes = getPlayers();
+		setTimeout(function(){
+		for (var i = 0; i < Object.keys(testes).length; i++) {
+			playerTableRow(testes[Object.keys(testes)[i]])
+		}
+	},400)
+}
+
+function playerTableRow (players){
+	var tr = document.createElement('tr');
+	console.log(players)
+	var nickname = players['Nome'].split(' ')[1]
+
+	var td = `<td>
+	    <div class="row">
+			<img src="../ProjectLOL/img/CBLOL/${players['Time'].split(' ')[0]}/${nickname}.png" class="rounded img-fluid img-player">
+			<div style="text-align: left;">
+			<label>${players['Nome'].split(' ')[0]} "<b class="text-info">${nickname}</b>" ${players['Nome'].split(' ')[2]} <br> <br>
+			Idade: ${players['Idade']} <br>Cidade: ${players['Cidade']}</label>
+		</div>
+	</td>
+	<td>
+		<div><img src="../ProjectLOL/img/Times/${players['Time'].split(' ')[0]}.png" class="rounded img-fluid" style="width:150px; height: 90px;"></div>
+	</td>
+	<td>
+		<div style="margin-left: 5%;"><img src="../ProjectLOL/img/Rotas/${players['Rota']}.png" class="border border-secondary" style="width:40px; height: 30px"></div>
+	</td>
+	`;
+	tr.innerHTML = td;
+
+	insertTable(tr);
+}
+
+function insertTable (tr){
+	var tbody = document.querySelector('#table-players tbody');
+	console.log(tbody)
+	tbody.appendChild(tr)
+}
